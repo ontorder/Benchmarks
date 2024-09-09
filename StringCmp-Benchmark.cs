7e1392a07ec21471f7b0df38255f4b5d4a225111
Luca Ciani <luvincste@libero.it>
2024-09-09 15:36:11 +0200

    Update StringCmp-Benchmark.cs

--- StringCmp-Benchmark.cs
+++ StringCmp-Benchmark.cs
@@ -3,6 +3,7 @@
 using BenchmarkDotNet.Attributes;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace test;
@@ -11,12 +12,14 @@ namespace test;
 public class bench_compare
 {
     private static readonly string _string1 = "ab0000";
-    private static readonly string _string2 = "ab111";
+    private static readonly string _string2 = "ab22";
+    private static readonly string[] _strings = ["ab0000", "ab1111", "ab22", "ab3333"];
 
     private static readonly byte[] _bytes1 = System.Text.Encoding.ASCII.GetBytes(_string1);
     private static readonly byte[] _bytes2 = System.Text.Encoding.ASCII.GetBytes(_string2);
+    private static readonly byte[][] _bytess = _strings.Select(System.Text.Encoding.ASCII.GetBytes).ToArray();
 
-    [Benchmark()]
+    //[Benchmark()]
     public bool GetBytesUnrolled()
     {
         var aBytes = System.Text.Encoding.ASCII.GetBytes(_string1);
@@ -28,16 +31,20 @@ public class bench_compare
     [Benchmark()]
     public bool BytesUnrolled()
     {
+        var ret = true;
+        foreach (var b in _bytess) ret &= CompareUnrolled4(_bytes2, b);
         return CompareUnrolled4(_bytes1, _bytes2);
     }
 
     [Benchmark()]
     public bool StringRegular()
     {
-        return _string1 != _string2;
+        var cmp = true;
+        foreach (var s in _strings) cmp &= _string2 != s;
+        return cmp;
     }
 
-    [Benchmark()]
+    //[Benchmark()]
     public bool StringNoCase()
     {
         return string.Compare(_string1, _string2, ignoreCase: true) == 0;
@@ -46,10 +53,15 @@ public class bench_compare
     [Benchmark()]
     public bool RegularBytes()
     {
-        bool iden = true;
-        var len = Math.Min(_bytes1.Length, _bytes2.Length);
-        for (int index = 0; iden && index < len; ++index) iden &= _bytes1[index] == _bytes2[index];
-        return iden;
+        var giden = true;
+        foreach (var b in _bytess)
+        {
+            var len = Math.Min(b.Length, _bytes2.Length);
+            bool iden = true;
+            for (int index = 0; iden && index < len; ++index) iden &= _bytes1[index] == _bytes2[index];
+            giden &= iden;
+        }
+        return giden;
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -95,5 +107,12 @@ public class bench_compare
 | BytesUnrolled    |  8.5182 ns | 0.0358 ns | 0.0318 ns |  8.5175 ns |      - |         - |
 | GetBytesUnrolled | 41.3684 ns | 0.2309 ns | 0.2047 ns | 41.3439 ns | 0.0102 |      64 B |
 | StringNoCase     | 42.1062 ns | 0.2677 ns | 0.2504 ns | 42.0516 ns |      - |         - |
+
+ciclo
+| Method        | Mean      | Error     | StdDev    | Allocated |
+|-------------- |----------:|----------:|----------:|----------:|
+| StringRegular |  3.532 ns | 0.0173 ns | 0.0153 ns |         - |
+| RegularBytes  | 21.210 ns | 0.1899 ns | 0.1683 ns |         - |
+| BytesUnrolled | 60.968 ns | 0.1751 ns | 0.1638 ns |         - |
 
 */
