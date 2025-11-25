ad388a069ee52d70881185f31465d78fe34b87e3
Luca Ciani <luvincste@libero.it>
2025-11-25 17:50:26 +0100

    Update Assign8ItemsLoop-Benchmark.cs

--- Assign8ItemsLoop-Benchmark.cs
+++ Assign8ItemsLoop-Benchmark.cs
@@ -1,21 +1,28 @@
+using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace test;
+
 [MemoryDiagnoser]
 public class bench_cycle
 {
     static cose[] coses = new cose[8]
     {
-        new cose{ rank=5 },
-        new cose{ rank=3 },
-        new cose{ rank=8 },
-        new cose{ rank=1 },
-        new cose{ rank=4 },
-        new cose{ rank=7 },
-        new cose{ rank=2 },
-        new cose{ rank=9 },
+        new(){ rank=5 },
+        new(){ rank=3 },
+        new(){ rank=8 },
+        new(){ rank=1 },
+        new(){ rank=4 },
+        new(){ rank=7 },
+        new(){ rank=2 },
+        new(){ rank=9 },
     };
 
-    static List<cose> coses_list = coses.ToList();
+    static List<cose> coses_list = [.. coses];
 
-    [Benchmark]
+    //[Benchmark]
     public List<cose> listforeach()
     {
         uint rank = 1;
@@ -23,7 +30,7 @@ public class bench_cycle
         return coses_list;
     }
 
-    [Benchmark]
+    //[Benchmark]
     public cose[] justfor()
     {
         for (uint rank = 1, id = 0; id < coses.Length; ++id) coses[id].rank = rank++;
@@ -31,6 +38,14 @@ public class bench_cycle
     }
 
     [Benchmark]
+    public cose[] spanfor()
+    {
+        var s = coses.AsSpan();
+        for (var id = 0; id < coses.Length; ++id) s[id].rank = (uint)id + 1;
+        return coses;
+    }
+
+    //[Benchmark]
     public cose[] justforeach()
     {
         uint rank = 1;
@@ -38,18 +53,25 @@ public class bench_cycle
         return coses;
     }
 
-    [Benchmark]
+    //[Benchmark]
     public cose[] linqselectlast()
     {
         _ = coses.Select(static (item, index) => item.rank = (uint)index).Last();
         return coses;
     }
 
-    [Benchmark]
+    //[Benchmark]
     public cose[] refloop()
     {
         uint rank = 1;
         foreach (ref var c in coses.AsSpan()) c.rank = rank++;
+        return coses;
+    }
+
+    //[Benchmark]
+    public cose[] innerselect()
+    {
+        foreach (var z in coses.Select((Item, Index) => (Item, Index))) z.Item.rank = (uint)z.Index;
         return coses;
     }
 }
@@ -61,11 +83,13 @@ public sealed class cose
 }
 
 /*
-| Method         | Mean      | Error     | StdDev    | Gen0   | Allocated |
-|--------------- |----------:|----------:|----------:|-------:|----------:|
-| refloop        |  3.793 ns | 0.0307 ns | 0.0287 ns |      - |         - |
-| justforeach    |  4.333 ns | 0.0266 ns | 0.0249 ns |      - |         - |
-| justfor        |  4.926 ns | 0.0448 ns | 0.0397 ns |      - |         - |
-| listforeach    | 24.771 ns | 0.1288 ns | 0.1076 ns | 0.0140 |      88 B |
-| linqselectlast | 84.164 ns | 0.5718 ns | 0.5069 ns | 0.0166 |     104 B |
+| Method         | Mean       | Error     | StdDev    | Gen0   | Allocated |
+|--------------- |-----------:|----------:|----------:|-------:|----------:|
+| refloop        |   3.793 ns | 0.0307 ns | 0.0287 ns |      - |         - |
+| justforeach    |   4.333 ns | 0.0266 ns | 0.0249 ns |      - |         - |
+| justfor        |   4.926 ns | 0.0448 ns | 0.0397 ns |      - |         - |
+| spanfor        |   4.965 ns | 0.0382 ns | 0.0358 ns |      - |         - |
+| listforeach    |  24.771 ns | 0.1288 ns | 0.1076 ns | 0.0140 |      88 B |
+| linqselectlast |  84.164 ns | 0.5718 ns | 0.5069 ns | 0.0166 |     104 B |
+| innerselect    | 117.2   ns | 0.83   ns | 0.77   ns | 0.0191 |     120 B |
 */
