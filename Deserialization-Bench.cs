b8fc7fbf2f7e9a0d364214637bd4c95d70a5674b
Luca Ciani <luvincste@libero.it>
2025-12-19 10:57:06 +0100

    Update Deserialization-Bench.cs

--- Deserialization-Bench.cs
+++ Deserialization-Bench.cs
@@ -10,7 +10,7 @@ public class bench_deserz
 {
     private const string json = @"{""Crap1"":123,""Crap2"":""much data"",""Crap3"":""01:02:03""}";
 
-    [Benchmark]
+    //[Benchmark]
     public Crap withStj()
     {
         return System.Text.Json.JsonSerializer.Deserialize<Crap>(json)!;
@@ -23,7 +23,7 @@ public class bench_deserz
         return default;
     }
 
-    [Benchmark]
+    //[Benchmark]
     public Crap withNewtonsoft()
     {
         return Newtonsoft.Json.JsonConvert.DeserializeObject<Crap>(json)!;
@@ -48,11 +48,37 @@ public class bench_deserz
         return manually;
     }
 
+    [Benchmark]
+    public Crap_fields manually2()
+    {
+        var split = json.Trim('{', '}').Split(',');
+        var manually = new Crap_fields();
+        foreach (var part in split)
+        {
+            var tokens = part.Split(':', 2);
+            var k = tokens[0].Trim('"');
+            switch (k)
+            {
+                case "Crap1": manually.Crap1 = int.Parse(tokens[1]); break;
+                case "Crap2": manually.Crap2 = tokens[1].Trim('"'); break;
+                case "Crap3": manually.Crap3 = TimeSpan.Parse(tokens[1].Trim('"')); break;
+            }
+        }
+        return manually;
+    }
+
     public sealed class Crap
     {
         public int Crap1 { get; set; }
         public string Crap2 { get; set; }
         public TimeSpan Crap3 { get; set; }
+    }
+
+    public sealed class Crap_fields
+    {
+        public int Crap1;
+        public string Crap2;
+        public TimeSpan Crap3;
     }
 }
 /*
@@ -60,6 +86,7 @@ public class bench_deserz
 | Method         | Mean     | Error   | StdDev  | Gen0   | Allocated |
 |--------------- |---------:|--------:|--------:|-------:|----------:|
 | withStj        | 382.3 ns | 1.10 ns | 0.92 ns | 0.0124 |      80 B |
+| manually2      | 417.9 ns | 1.84 ns | 1.72 ns | 0.1540 |     968 B |
 | manually       | 433.0 ns | 2.53 ns | 2.25 ns | 0.1540 |     968 B |
 | withNewtonsoft | 870.8 ns | 7.91 ns | 7.40 ns | 0.4387 |    2752 B |
 
