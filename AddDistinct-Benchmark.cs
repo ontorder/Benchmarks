ea48d8f982f83a127a41d5e2a7614a3203779a44
Luca Ciani <luvincste@libero.it>
2026-01-28 22:08:06 +0100

    Update AddDistinct-Benchmark.cs

--- AddDistinct-Benchmark.cs
+++ AddDistinct-Benchmark.cs
@@ -7,7 +7,7 @@ using System.Linq;
 namespace test;
 
 [MemoryDiagnoser]
-//[SimpleJob(BenchmarkDotNet.Engines.RunStrategy.ColdStart)]
+[SimpleJob(BenchmarkDotNet.Engines.RunStrategy.ColdStart)]
 public class bench_distinct
 {
     [Benchmark]
@@ -50,7 +50,7 @@ public class bench_distinct
     public string[] frozen_distinct()
     {
         var frozen = ImmutableSortedSet.Create<string>();
-        foreach (var term in enumterms()) frozen.Add(term);
+        foreach (var term in enumterms()) frozen = frozen.Add(term);
         return [.. frozen.Distinct()];
     }
 
@@ -58,42 +58,44 @@ public class bench_distinct
     {
         yield return "trinité"; yield return "montagna"; yield return "hotel"; yield return "salle"; yield return "fuochi"; yield return "artificio"; yield return "montagna";
         yield return "hotel"; yield return "comune"; yield return "aymavilles";
-        //yield return "aymavilles"; yield return "elicottero"; yield return "fuochi";
-        //yield return "cervo"; yield return "stambecco"; yield return "polenta"; yield return "lard"; yield return "hone"; yield return "guardia"; yield return "montagna";
+        yield return "aymavilles"; yield return "elicottero"; yield return "fuochi";
+        yield return "cervo"; yield return "stambecco"; yield return "polenta"; yield return "lard"; yield return "hone"; yield return "guardia"; yield return "montagna";
 
-        //yield return "volpe"; yield return "cervo"; yield return "aosta"; yield return "sarre"; yield return "macchina"; yield return "fotografia"; yield return "video";
-        //yield return "volpe"; yield return "brasato"; yield return "costa"; yield return "collina"; yield return "sole"; yield return "mare"; yield return "video";
-        //yield return "quindici"; yield return "gennaio"; yield return "inverno"; yield return "estate"; yield return "primavera"; yield return "mare";
+        yield return "volpe"; yield return "cervo"; yield return "aosta"; yield return "sarre"; yield return "macchina"; yield return "fotografia"; yield return "video";
+        yield return "volpe"; yield return "brasato"; yield return "costa"; yield return "collina"; yield return "sole"; yield return "mare"; yield return "video";
+        yield return "quindici"; yield return "gennaio"; yield return "inverno"; yield return "estate"; yield return "primavera"; yield return "mare";
 
-        //yield return "volpe1"; yield return "cervo1"; yield return "aosta1"; yield return "sarre1"; yield return "macchina1"; yield return "fotografia1"; yield return "video";
-        //yield return "volpe1"; yield return "brasato1"; yield return "costa1"; yield return "collina1"; yield return "sole1"; yield return "mare1"; yield return "video";
-        //yield return "quindici1"; yield return "gennaio1"; yield return "inverno1"; yield return "estate1"; yield return "primavera1"; yield return "mare";
+        yield return "volpe1"; yield return "cervo1"; yield return "aosta1"; yield return "sarre1"; yield return "macchina1"; yield return "fotografia1"; yield return "video";
+        yield return "volpe1"; yield return "brasato1"; yield return "costa1"; yield return "collina1"; yield return "sole1"; yield return "mare1"; yield return "video";
+        yield return "quindici1"; yield return "gennaio1"; yield return "inverno1"; yield return "estate1"; yield return "primavera1"; yield return "mare";
 
-        //yield return "1volpe"; yield return "1cervo"; yield return "1aosta"; yield return "1sarre"; yield return "1macchina"; yield return "1fotografia"; yield return "video";
-        //yield return "1volpe"; yield return "1brasato"; yield return "1costa"; yield return "1collina"; yield return "1sole"; yield return "1mare"; yield return "video";
-        //yield return "1quindici"; yield return "gennaio"; yield return "1inverno"; yield return "1estate"; yield return "primavera"; yield return "mare";
+        yield return "1volpe"; yield return "1cervo"; yield return "1aosta"; yield return "1sarre"; yield return "1macchina"; yield return "1fotografia"; yield return "video";
+        yield return "1volpe"; yield return "1brasato"; yield return "1costa"; yield return "1collina"; yield return "1sole"; yield return "1mare"; yield return "video";
+        yield return "1quindici"; yield return "gennaio"; yield return "1inverno"; yield return "1estate"; yield return "primavera"; yield return "mare";
     }
 }
 /*
-| 10 items            | Mean       | Error    | StdDev   | Gen0   | Allocated |
-|-------------------- |-----------:|---------:|---------:|-------:|----------:|
-| frozen_distinct     |   341.1 ns |  6.85 ns | 12.52 ns | 0.1564 |     984 B |
-| linq_distinct       |   355.9 ns |  2.47 ns |  2.31 ns | 0.1502 |     944 B |
-| sorted_set_distinct | 1,358.8 ns | 27.23 ns | 35.40 ns | 0.1755 |    1104 B |
-| builder_distinct    | 1,587.6 ns | 17.06 ns | 14.25 ns | 0.1621 |    1024 B |
-| order_distinct      | 1,949.4 ns | 15.48 ns | 14.48 ns | 0.2480 |    1576 B |
-| sort_distinct       | 1,974.3 ns | 21.75 ns | 20.34 ns | 0.1411 |     888 B |
-| immutable_distinct  | 2,723.3 ns | 26.71 ns | 24.98 ns | 0.2136 |    1352 B |
+| 10 items            | Mean       | Error    | StdDev   | Median     | Gen0   | Allocated |
+|-------------------- |-----------:|---------:|---------:|-----------:|-------:|----------:|
+| linq_distinct       |   349.0 ns |  3.99 ns |  3.73 ns |   350.2 ns | 0.1502 |     944 B |
+| sorted_set_distinct | 1,344.9 ns | 26.56 ns | 42.89 ns | 1,321.6 ns | 0.1755 |    1104 B |
+| builder_distinct    | 1,611.9 ns | 15.54 ns | 14.54 ns | 1,611.9 ns | 0.1621 |    1024 B |
+| frozen_distinct     | 1,829.5 ns | 12.70 ns | 11.88 ns | 1,827.5 ns | 0.3242 |    2040 B |
+| order_distinct      | 1,957.5 ns | 15.77 ns | 14.75 ns | 1,958.8 ns | 0.2480 |    1576 B |
+| sort_distinct       | 2,017.4 ns | 27.29 ns | 25.53 ns | 2,020.4 ns | 0.1411 |     888 B |
+| immutable_distinct  | 2,732.3 ns | 30.06 ns | 25.10 ns | 2,731.2 ns | 0.2136 |    1352 B |
 
 | 10 items cold start | Mean     | Error     | StdDev    | Median    | Allocated |
 |-------------------- |---------:|----------:|----------:|----------:|----------:|
-| linq_distinct       | 16.37 us |  25.37 us |  74.80 us |  7.700 us |   1.31 KB |
-| order_distinct      | 24.23 us |  31.76 us |  93.64 us | 12.800 us |   1.93 KB |
-| sort_distinct       | 27.86 us |  37.13 us | 109.46 us | 12.100 us |   1.26 KB |
-| frozen_distinct     | 32.07 us |  76.64 us | 225.98 us |  6.550 us |   1.35 KB |
-| sorted_set_distinct | 34.34 us |  61.29 us | 180.72 us | 13.700 us |   1.47 KB |
-| immutable_distinct  | 62.24 us | 111.65 us | 329.20 us | 22.200 us |   1.71 KB |
-| builder_distinct    | 67.21 us | 167.28 us | 493.22 us | 14.900 us |   1.39 KB |
+| linq_distinct       | 13.71 us |  24.79 us |  73.11 us |  5.800 us |   1.31 KB |
+| order_distinct      | 22.03 us |  29.98 us |  88.39 us | 11.650 us |   1.93 KB |
+| sort_distinct       | 22.22 us |  34.12 us | 100.59 us | 11.200 us |   1.26 KB |
+| sorted_set_distinct | 28.07 us |  50.90 us | 150.09 us | 11.500 us |   1.47 KB |
+| frozen_distinct     | 43.16 us |  97.64 us | 287.89 us | 12.700 us |   2.38 KB |
+| builder_distinct    | 46.66 us | 104.68 us | 308.66 us | 12.400 us |   1.39 KB |
+| immutable_distinct  | 54.19 us | 116.53 us | 343.60 us | 16.900 us |   1.71 KB |
+
+                REDO oopsie
 
 | 20 items            | Mean       | Error    | StdDev   | Gen0   | Allocated |
 |-------------------- |-----------:|---------:|---------:|-------:|----------:|
@@ -122,11 +124,11 @@ public class bench_distinct
 
 | 80 items cold start | Mean      | Error     | StdDev    | Median    | Allocated |
 |-------------------- |----------:|----------:|----------:|----------:|----------:|
-| linq_distinct       |  21.48 us |  36.16 us | 106.61 us |  9.800 us |   4.29 KB |
-| frozen_distinct     |  44.53 us | 101.01 us | 297.83 us | 13.200 us |   6.82 KB |
-| order_distinct      |  54.17 us |  44.92 us | 132.44 us | 39.000 us |      7 KB |
-| sort_distinct       |  57.03 us |  46.59 us | 137.38 us | 41.100 us |   4.74 KB |
-| sorted_set_distinct |  76.05 us |  89.65 us | 264.34 us | 45.900 us |   5.57 KB |
-| builder_distinct    | 113.96 us | 162.60 us | 479.44 us | 57.450 us |   5.46 KB |
-| immutable_distinct  | 118.52 us | 185.18 us | 546.00 us | 58.050 us |    7.6 KB |
+| linq_distinct       |  24.10 us |  35.47 us | 104.58 us |  9.600 us |   4.29 KB |
+| order_distinct      |  67.94 us |  41.51 us | 122.41 us | 51.650 us |      7 KB |
+| sorted_set_distinct |  71.64 us |  66.71 us | 196.69 us | 49.800 us |   5.57 KB |
+| sort_distinct       |  86.96 us | 102.70 us | 302.81 us | 51.400 us |   4.74 KB |
+| immutable_distinct  | 103.51 us | 121.31 us | 357.68 us | 62.900 us |    7.6 KB |
+| builder_distinct    | 104.24 us | 116.00 us | 342.04 us | 69.450 us |   5.46 KB |
+| frozen_distinct     | 120.91 us | 109.30 us | 322.28 us | 77.000 us |  20.67 KB |
 */
